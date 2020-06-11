b5cddbdb65d80d7684fc75bde063022e2aeb623c
shubham shah <shubham.shah@dish.com>
2020-06-11 23:56:50 +0530

    Day 1 Completed

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Shah, Shubham on 11/06/20.
+//  Copyright © 2020 Shubham shah. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var name: UILabel!
+}
---
+++ NamesToFaces/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  NamesToFaces
+//
+//  Created by Shah, Shubham on 11/06/20.
+//  Copyright © 2020 Shubham shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue Person Cell")
+        }
+        return cell
+    }
+
+}
+
